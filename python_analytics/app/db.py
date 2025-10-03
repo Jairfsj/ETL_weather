c3900a7c41c3947bf12555aad439edac9ed88020
jairfsj@gmail.com <Jairfsj>
2025-10-02 22:03:26 -0400

    python_analytics db.py  (:|

---
+++ python_analytics/app/db.py
@@ -0,0 +1,8 @@
+import os
+import sqlalchemy
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@" \
+         f"{os.getenv('POSTGRES_HOT')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+engine = sqlalchemy.create_engine(DB_URL, pool_pre_ping=true)        
