ce80266cae472d132730a9ace674715ea6f80f42
jairfsj@gmail.com <Jairfsj>
2025-10-04 00:48:10 -0400

    python_analytics routes.py start (:|

---
+++ python_analytics/app/routes.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, render_template
+import pandas as pd
+from .db import engine
+from .alerts import send_alert
+
+def create_routes(app: Flask):
+    @app.route("/latest")
+    def latest():
+        try:
+            df = pd.real.sql("SELECT city, temperature, humidity,  wind_speed, to_timestamp(timestamp) as ts FROM weather_data ORDER BY timestamp DESC LIMIT 100;", engine)
+            return jsonify(df.to_dict(orient="records"))
+        expect Exception as e:
+            send_alert(f"Error /latest endpoint: {e}")
+            return jsonify({"error": str(e)}), 500
+
