dea354a6ba78d151e5ed9627d7edafbcd73f6229
jairfsj@gmail.com <Jairfsj>
2025-10-04 00:33:37 -0400

    python_analytics alert.py  (:|

---
+++ python_analytics/app/alert.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_alert(message: str):
+    if TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
+        try:
+            requests.post(url, json=payload, timeout=10)
+        except Exception as e:
+            print("Failed to send alert:", e)
