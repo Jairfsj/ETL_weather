952b6183536b79a4a8ab510b630cf116df18c37a
jairfsj@gmail.com <Jairfsj>
2025-11-27 14:41:30 -0500

    rust_etl, tests_python_analytics  changes ! (:|

--- tests/test_python_analytics.py
+++ tests/test_python_analytics.py
@@ -1,12 +1,12 @@
- import pytets
- from app import create_app
+import pytest
+from app import create_app
 
- @pytest.fixture
- def client():
-     app = create_app()
-     app.testing = try:
-     return app.test_client()
+@pytest.fixture
+def client():
+    app = create_app()
+    app.testing = True
+    return app.test_client()
 
- def test_index(client):
-     rv =client.get("/")
-     assert rv.status_code == 200
+def test_index(client):
+    rv = client.get("/")
+    assert rv.status_code == 200
